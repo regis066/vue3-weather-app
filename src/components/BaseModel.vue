e24a8db86deb9cb7927683fedab5261c12ec8908
Regis066 <mugisharegis72@gmail.com>
2023-10-25 21:54:34 +0200

    build the navigation component

---
+++ src/components/BaseModel.vue
@@ -0,0 +1,26 @@
+<template>
+    <div
+    v-show="modalActive"
+    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
+        <div
+        v-if="modalActive"
+        class="p-4 bg-white self-start mt-32 max-w-screen-md">
+            <slot />
+            <button class="text-white mt-8 bg-weather-primary py-2 px-6" @click="$emit('close-modal')">Close</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineEmits(['close-modal'])
+defineProps({
+    modalActive: {
+        type: Boolean,
+        default: false,
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
